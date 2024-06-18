e0879a7553ce8610530bd781a6de9fb038b28420
Ruan Da Silva Sathler <133224756+RuanSathler@users.noreply.github.com>
2024-06-18 00:55:52 -0300

    Add files via upload

---
+++ treino.py
@@ -0,0 +1,12 @@
+#collect data from an employee
+name = input("enter the name of the employee: ")
+code = int(input("enter the cod of the employee: "))
+age = int(input("enter the age of the employee: "))
+wage = float(input("enter the wage of the employee: "))
+
+print("the date of the employee ", name, "is...")
+print("code", code)
+print("age", age)
+print("wage", wage)
+
+
